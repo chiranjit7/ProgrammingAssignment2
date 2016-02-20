0598f5e6ef81e3a925541939653b59b1c84b55f3
chiranjit7 <chiranjit7@gmail.com>
2016-02-20 23:07:58 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# this is to calculate the matrix functions which cache the inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function(){
+    x
+  } 
+  setmatrix<-function(inverse){ #Feel comfortable in building functions in this way
+    m <<- inverse
+  } 
+  getmatrix<-function(){
+    m
+  }  
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+  m <- x$getmatrix()
+  if(!is.null(m) & !is.character(m)) { #m can be character in my code, so you see an aditional command here
+    message("getting cached data")
+    return(m)
+  }
+  matrix <-x$get() # I feel it is important to have the error printing for this code
+  if (is.character(try(solve(matrix, ...),T))==T){
+      m <- x$setmatrix("Most probably your matrix is either not a square matrix or it is not invertible; get the details from http://www.netlib.org/lapack") 
+    } else {
+      m <-solve(matrix, ...)
+      x$setmatrix(m)     
+    }
+  m
 }
